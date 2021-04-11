7a7b1534da034258e34cd480f8b118096f103969
Stuart McCamley <s.mccamley@gmail.com>
2021-04-11 22:08:31 +0100

    done

--- src/axidraw/run.ts
+++ src/axidraw/run.ts
@@ -1,20 +1,21 @@
-import {run} from 'node-cmd'
+import { run } from "node-cmd";
 
 //axicli cookies.svg --speed_pendown 100 --speed_penup 100 --accel 100 --pen_pos_down 8 --pen_pos_up 36 --pen_rate_lower 100 --pen_rate_raise 100 --pen_delay_down 0 --pen_delay_up 0
 
-export const plot = ({filename}:{filename:string}) => {
- 
-    run(`axicli ./src/axidraw/${filename} --speed_pendown 100 --speed_penup 100 --accel 100 --pen_pos_down 8 --pen_pos_up 36 --pen_rate_lower 100 --pen_rate_raise 100 --pen_delay_down 0 --pen_delay_up 0`, (err, data, stderr)=>{
-        console.log(err, data, stderr)
-    })
-}
+export const plot = ({ filename }: { filename: string }) => {
+  run(
+    `axicli ./src/axidraw/${filename} --speed_pendown 50 --speed_penup 50 --accel 70 --pen_pos_down 8 --pen_pos_up 36 --pen_rate_lower 100 --pen_rate_raise 100 --pen_delay_down 0 --pen_delay_up 0`,
+    (err, data, stderr) => {
+      console.log(err, data, stderr);
+    }
+  );
+};
 
 export const disable = () => {
-  run(`axicli --mode manual --manual_cmd disable_xy`)
-}
-
+  run(`axicli --mode manual --manual_cmd disable_xy`);
+};
 
 export default {
-    plot,
-    disable
-}
+  plot,
+  disable,
+};
